788e8302ca2e41c4b4a2245865994c36970396bf
Luke Chen <luke.chen.2010@gmail.com>
2020-07-19 03:08:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Write your code here!
 
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
